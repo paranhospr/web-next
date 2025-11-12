d3c8c1ca7f1b194317d4b66841b49d8ccb674b22
paranhospr <paranhospr@gmail.com>
2025-11-12 06:41:50 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
 export const revalidate = 0;
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
